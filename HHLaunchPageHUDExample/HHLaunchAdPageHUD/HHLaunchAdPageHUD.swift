77251347e94f774a0c22c06a8313ca7178ddc297
ethan_xue <ethan_xue@foxmail.com>
2018-01-07 17:30:07 +0800

    +初始化工程

---
+++ HHLaunchPageHUDExample/HHLaunchAdPageHUD/HHLaunchAdPageHUD.swift
@@ -0,0 +1,21 @@
+//
+//  HHLaunchAdPageHUD.swift
+//  HHLaunchPageHUDExample
+//
+//  Created by 薛乐 on 2018/1/7.
+//  Copyright © 2018年 ethan. All rights reserved.
+//
+
+import UIKit
+
+class HHLaunchAdPageHUD: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ HHLaunchPageHUDExample/HHLaunchPageHUDExample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HHLaunchPageHUDExample
+//
+//  Created by 薛乐 on 2018/1/7.
+//  Copyright © 2018年 ethan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
